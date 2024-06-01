faab869229dbbb1a5e75287c1c946a5ac67a8a93
Francesco-Campagna <francesco.campa2010@libero.it>
2024-06-01 18:32:59 +0200

    FrontEnd:
    * modified access page
    * created log in html
    * created sign up html

--- src/app/componenti/registration/registration.component.ts
+++ src/app/componenti/registration/registration.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements AfterViewInit{
+
+  registrationSuccess: boolean = false;
+
+  ngAfterViewInit(): void {
+    document.getElementById('signupForm')?.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+    });
+  }
+
+  signup(registrationSuccess: boolean){
+    if (registrationSuccess)this.registrationSuccess = true;
+    const messageContainer = document.getElementById('messageContainer')!;
+
+    if (registrationSuccess) {
+      this.showMessage(messageContainer, 'Registration successful!', 'success');
+    } else {
+      this.showMessage(messageContainer, 'Error during account registration. Retry.', 'error');
+    }
+  }
+
+
+  showMessage(container: HTMLElement, message: string, type: 'success' | 'error') {
+    container.textContent = message;
+    container.className = `message ${type} show`;
+  }
+
+  checkRegistration(){
+    const tmp = Math.random();
+    if (Math.floor(tmp * 2) % 2 === 0){
+      this.signup(true);
+    }else{
+      this.signup(false);
+    }
+  }
 
 }
