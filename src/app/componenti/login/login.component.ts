cdde462f4fd7f662f80fb657dc2f32fe56493446
Francesco-Campagna <francesco.campa2010@libero.it>
2024-06-27 16:09:33 +0200

    FrontEnd: Login and Register
    * created handler for login and register

--- src/app/componenti/login/login.component.ts
+++ src/app/componenti/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router, withPreloading} from "@angular/router";
+import {AuthService} from "../../auth/auth.service";
+import {LoginRequestDto} from "../../Model/LoginRequestDto";
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,38 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  constructor() { }
+  email: string | undefined;
+  password : string | undefined;
+  errorMessage = 'Invalid Credentials';
+  successMessage: string = "Success login";
+  invalidLogin = false;
+  loginSuccess = false;
 
-  login(){
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthService) {   }
 
+
+  handleLogin() {
+    if (this.email && this.password){
+      const loginDto = new LoginRequestDto();
+      loginDto.email = this.email;
+      loginDto.password = this.password;
+      this.authenticationService.login(loginDto).subscribe((result)=> {
+        this.invalidLogin = false;
+        this.loginSuccess = true;
+        this.successMessage = 'Login Successful.';
+        this.router.navigate(['/home']).then(() => {
+          window.location.reload();
+        });
+      }, () => {
+        this.invalidLogin = true;
+        this.loginSuccess = false;
+      });
+    } else {
+      this.invalidLogin = true;
+    }
   }
 
 }
--- src/app/componenti/registration/registration.component.ts
+++ src/app/componenti/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import {AfterViewInit, Component} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {RegistrationRequestDto} from "../../Model/RegistrationRequestDto";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registration',
@@ -7,7 +10,17 @@ import {AfterViewInit, Component} from '@angular/core';
 })
 export class RegistrationComponent implements AfterViewInit{
 
+  name: string | undefined;
+  surname: string | undefined;
+  email: string | undefined;
+  phone: string | undefined;
+  password : string | undefined;
   registrationSuccess: boolean = false;
+
+
+
+  constructor(private authenticationService: AuthService, private router: Router) {
+  }
 
   ngAfterViewInit(): void {
     document.getElementById('signupForm')?.addEventListener('submit', function(event) {
@@ -16,29 +29,33 @@ export class RegistrationComponent implements AfterViewInit{
     });
   }
 
-  signup(registrationSuccess: boolean){
-    if (registrationSuccess)this.registrationSuccess = true;
-    const messageContainer = document.getElementById('messageContainer')!;
-
-    if (registrationSuccess) {
-      this.showMessage(messageContainer, 'Registration successful!', 'success');
-    } else {
-      this.showMessage(messageContainer, 'Error during account registration. Retry.', 'error');
-    }
-  }
-
 
   showMessage(container: HTMLElement, message: string, type: 'success' | 'error') {
     container.textContent = message;
     container.className = `message ${type} show`;
   }
 
-  checkRegistration(){
-    const tmp = Math.random();
-    if (Math.floor(tmp * 2) % 2 === 0){
-      this.signup(true);
-    }else{
-      this.signup(false);
+  handleRegistration(){
+    if (this.name && this.surname && this.email && this.phone && this.password) {
+      const registrationDto = new RegistrationRequestDto();
+      registrationDto.name = this.name;
+      registrationDto.surname = this.surname;
+      registrationDto.email = this.email;
+      registrationDto.phone = this.phone;
+      registrationDto.password = this.password;
+      this.authenticationService.register(registrationDto).subscribe(
+        (result) => {
+          this.registrationSuccess = true;
+          this.router.navigate(['/login']);
+        },
+        () => {
+          this.registrationSuccess = false;
+          // Puoi gestire eventuali errori di registrazione qui
+        }
+      );
+    } else {
+      // Gestione dei campi mancanti per la registrazione
+      // Puoi impostare un flag o un messaggio di errore appropriato qui
     }
   }
 
